3d5a3ffcda181001e430e3427a73fc2728cef973
Lisandro Sebastian Scarrone <lisandro.sebsca@gmail.com>
2020-08-20 12:22:52 -0300

    Mejora legibilidad
    Cambio de nombre en variables para mejorar la legibilidad del código.

--- traductor.py
+++ traductor.py
@@ -1,36 +1,37 @@
 from syl_module import silabizer
 
 s = silabizer()
-p = "ahora estoy testeando la letra y"
-l = s(p)
-print(l)
-res = ""
+palabra = "estoy hablando en jeringoso y probando la letra y"
+palabraEnSilabas = s(palabra)
+print(palabraEnSilabas)
+palabraEnJeringoso = ""
 
-for sil in l:
-	ger = str(sil).strip()
-	#print(ger)
-	if "a" in ger:
-		res += ger
-		res += "pa "
+for silaba in palabraEnSilabas:
+	stringSilaba = str(silaba).strip()
+	#print(stringSilaba)
+	if "a" in stringSilaba:
+		palabraEnJeringoso += stringSilaba
+		palabraEnJeringoso += "pa "
 
-	elif "e" in ger:
-		res += ger
-		res += "pe "
+	elif "e" in stringSilaba:
+		palabraEnJeringoso += stringSilaba
+		palabraEnJeringoso += "pe "
 		
-	elif "i" in ger:
-		res += ger
-		res += "pi "
+	elif "i" in stringSilaba:
+		palabraEnJeringoso += stringSilaba
+		palabraEnJeringoso += "pi "
 		
-	elif "o" in ger:
-		res += ger
-		res += "po "
+	elif "o" in stringSilaba:
+		palabraEnJeringoso += stringSilaba
+		palabraEnJeringoso += "po "
 		
-	elif "u" in ger:
-		res += ger
-		res += "pu "
+	elif "u" in stringSilaba:
+		palabraEnJeringoso += stringSilaba
+		palabraEnJeringoso += "pu "
 
-	elif "y" in ger:
-		res += ger
-		res += "pi "
+	elif "y" in stringSilaba:
+		palabraEnJeringoso += stringSilaba
+		palabraEnJeringoso += "pi "
 
-print(res)
+
+print(palabraEnJeringoso)
